4934387aa424b7584320d0512c9840d19e519b4d
SupremeEagl3 <75276668+SupremeEagl3@users.noreply.github.com>
2024-04-04 00:57:43 +0000

    Pending changes exported from your codespace

---
+++ teste.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+    int main()
+    {
+        int v1, v2;
+        cout<<"Digite o primeiro valor inteiro: ";
+        cin>>v1;
+        cout<<"Digite o segundo valor inteiro: ";
+        cin>>v2;
+
+        if (v1>v2)
+        {
+            cout<<"A adicao dos valores eh: "<<v1+v2<<endl;
+        }else
+        {
+            cout<<"A multiplicacao dos valores eh: "<<v1*v2<<endl;
+        }
+        
+        
+        return main();
+    }
+    
