d80829942292336ffb0bcacec2854621178afd5f
ciph3r1 <165969455+ciph3r1@users.noreply.github.com>
2024-04-07 19:07:46 -0300

    Update Projeto.cpp

--- Projeto.cpp
+++ Projeto.cpp
@@ -5,40 +5,30 @@ using namespace std;
     {
        double senha;
        
-       cout<<"Para acessar ao sistema, digite a senha de acesso:\n";
+       cout<<"Para acessar ao sistema, digite a senha de acesso:\n"<<endl;
        cin>>senha;
        
        if(senha == 2024121867 || senha == 2024121325 || senha == 2024121227)
        {
-            string nome, endereço, cpf, tipo;
+            string nome, endereço, cpf;
+            int tipo;
             int dia,mes,ano;
             
-            cout<<"Login efetuado com sucesso"<<endl;
+            cout<<"\nLogin efetuado com sucesso"<<endl;
             cout<<"Seja bem vindo ao Cadastro de Funcionários"<<endl;
-            cout<<"Será necessário realizar um novo registro"<<endl;
+            cout<<"Será necessário realizar um novo registro\n\n"<<endl;
             
             cout<<"Informe o seu nome completo:"<<endl;
              cin>>nome;
             cout<<"Informe o CPF do funcionario a ser registrado:"<<endl;
              cin>>cpf;
-            cout<<"Informe o seu Endereço completo:"<<endl;]
+            cout<<"Informe o seu Endereço completo:"<<endl;
              cin>>endereço;
-            cout>>"Informe o tipo de Funcionario ao qual voce foi cadastrado:\n(efetivo, temporario ou estagiario)"<<endl;
+            cout<<"Informe o tipo de Funcionario ao qual voce foi cadastrado, digite 1 para efetivo, 2 para temporario e 3 para estagiario: "<<endl;
              cin>>tipo;
-            cout>>"Digite a data de admissão: (Eh necessario digitar no formato DD MM AAAA separado por espaços)"
-             cin<<dia<<mes<<ano;
-            
-        switch(tipo)
-        {
-            double salario_base, hr_extra,
-            {
-            case 'efetivo':
-            case 'Efetivo':
-            }
-        }
+            cout<<"Digite a data de admissão:"<<endl;
+             cin>>dia>>mes>>ano;
        }
-        
         return 0;
     }
     
-
