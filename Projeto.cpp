1e137144155930ab7d604fe58d1d08fafac3d038
ciph3r1 <165969455+ciph3r1@users.noreply.github.com>
2024-04-07 18:59:24 -0300

    Update Projeto.cpp

--- Projeto.cpp
+++ Projeto.cpp
@@ -1,26 +1,44 @@
-
 #include <iostream>
-
 using namespace std;
 
     int main()
     {
-        int v1, v2;
-        cout<<"Digite o primeiro valor inteiro: ";
-        cin>>v1;
-        cout<<"Digite o segundo valor inteiro: ";
-        cin>>v2;
-
-        if (v1>v2)
+       double senha;
+       
+       cout<<"Para acessar ao sistema, digite a senha de acesso:\n";
+       cin>>senha;
+       
+       if(senha == 2024121867 || senha == 2024121325 || senha == 2024121227)
+       {
+            string nome, endereço, cpf, tipo;
+            int dia,mes,ano;
+            
+            cout<<"Login efetuado com sucesso"<<endl;
+            cout<<"Seja bem vindo ao Cadastro de Funcionários"<<endl;
+            cout<<"Será necessário realizar um novo registro"<<endl;
+            
+            cout<<"Informe o seu nome completo:"<<endl;
+             cin>>nome;
+            cout<<"Informe o CPF do funcionario a ser registrado:"<<endl;
+             cin>>cpf;
+            cout<<"Informe o seu Endereço completo:"<<endl;]
+             cin>>endereço;
+            cout>>"Informe o tipo de Funcionario ao qual voce foi cadastrado:\n(efetivo, temporario ou estagiario)"<<endl;
+             cin>>tipo;
+            cout>>"Digite a data de admissão: (Eh necessario digitar no formato DD MM AAAA separado por espaços)"
+             cin<<dia<<mes<<ano;
+            
+        switch(tipo)
         {
-            cout<<"A adicao dos valores eh: "<<v1+v2<<endl;
-        }else
-        {
-            cout<<"A multiplicacao dos valores eh: "<<v1*v2<<endl;
+            double salario_base, hr_extra,
+            {
+            case 'efetivo':
+            case 'Efetivo':
+            }
         }
+       }
         
-        
-        return main();
-        system ("cls");
+        return 0;
     }
     
+
